cb55d293c086cb8844dd2301b4f1777723624029
tsudakanako <167269340+tsudakanako@users.noreply.github.com>
2024-06-19 14:41:49 +0900

    kadai_004をアップロード

---
+++ kadai_004/event.js
@@ -0,0 +1,11 @@
+
+  //読み込み完了したとき
+  $(window).on('load', () => {
+    console.log('loadイベントが発生しました');
+  });
+
+
+  //画面をスクロールしたとき
+  $(window).on('scroll', () => {
+    console.log('scrollイベントが発生しました');
+  });
