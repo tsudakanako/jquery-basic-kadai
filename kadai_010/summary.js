ce720d5edf3ca7de41f5829c586095ba185da6b5
tsudakanako <167269340+tsudakanako@users.noreply.github.com>
2024-06-22 00:03:35 +0900

    kadai_010をアップロード

---
+++ kadai_010/summary.js
@@ -0,0 +1,18 @@
+$(function() {
+//色が変わる
+  $('#change-color').on('click', function(){
+    $('#target').css('color', 'red');
+  });
+//文字が切り替わる
+  $('#change-text').on('click', function(){
+    $('#target').text('こんばんは！');
+    });
+//フェードアウトで文字が消える
+  $('#fade-out').on('click', function(){
+    $('#target').fadeOut();
+    });
+//フェードインで文字が現れる
+  $('#fade-in').on('click', function(){
+    $('#target').fadeIn();
+    });
+});
